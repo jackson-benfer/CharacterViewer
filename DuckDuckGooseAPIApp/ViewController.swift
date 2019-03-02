e8246f1f0cdb7aa6ceb25d5f40d371d191fb2014
MAC Consultant <consultant@MacBook-Pro-10.local>
2019-03-01 20:15:55 -0500

    Initial Commit

---
+++ DuckDuckGooseAPIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DuckDuckGooseAPIApp
+//
+//  Created by MAC Consultant on 3/1/19.
+//  Copyright © 2019 William Benfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
